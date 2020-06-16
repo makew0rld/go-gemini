9d7ac4323c2ba8010d14bcfce6bca1b4477bb2ea
makeworld <colecmac@protonmail.com>
2020-06-16 19:05:31 -0400

    💩 Will this fix EOF errors?

--- client.go
+++ client.go
@@ -97,10 +97,15 @@ func (c *Client) connect(res *Response, parsedURL *url.URL) (io.ReadWriteCloser,
 		return conn, err
 	}
 
+	if err = conn.Handshake(); err != nil {
+		return nil, err
+	}
+
 	cert := conn.ConnectionState().PeerCertificates[0]
 	res.Cert = cert
 
 	if c.Insecure {
+		// No further checks
 		return conn, nil
 	}
 
@@ -108,14 +113,17 @@ func (c *Client) connect(res *Response, parsedURL *url.URL) (io.ReadWriteCloser,
 	if !c.NoHostnameCheck {
 		err := cert.VerifyHostname(parsedURL.Hostname())
 		if err != nil {
+			conn.Close()
 			return nil, fmt.Errorf("hostname does not verify: %v", err)
 		}
 	}
 	// Verify expiry
 	if !c.NoTimeCheck {
 		if cert.NotBefore.After(time.Now()) {
+			conn.Close()
 			return nil, fmt.Errorf("server cert is for the future")
 		} else if cert.NotAfter.Before(time.Now()) {
+			conn.Close()
 			return nil, fmt.Errorf("server cert is expired")
 		}
 	}
